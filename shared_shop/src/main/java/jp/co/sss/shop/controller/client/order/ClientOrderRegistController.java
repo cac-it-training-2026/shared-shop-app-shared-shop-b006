4755da7e1a1a9314e36753060a135ca3ef0e5788
takatos <takatos@C010423335.zipangu.cac.co.jp>
2026-06-08 17:25:28 +0900

    add:フォームのrequest、sessionへの登録と削除、入力チェック変数の定義と条件分岐の作成

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -2,14 +2,20 @@ package jp.co.sss.shop.controller.client.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.servlet.http.HttpSession;
 import jp.co.sss.shop.form.OrderForm;
 import jp.co.sss.shop.repository.OrderRepository;
 
 @Controller
+@SessionAttributes(value = "orderForm")
 public class ClientOrderRegistController {
 
 	@Autowired
@@ -24,13 +30,17 @@ public class ClientOrderRegistController {
 
 	}
 
-	//	
-	//	@RequestMapping(path="/client/order/address/input",method=RequestMethod.POST)
-	//	String addressInput() {
-	//		return "redirect:/client/order/address/input ";
-	//	}
-	@RequestMapping(path = "/client/order/address/input")
+	@RequestMapping(path = "/client/order/address/input", method = RequestMethod.POST)
 	String addressInput() {
+		return "redirect:/client/order/address/input ";
+	}
+
+	@RequestMapping(path = "/client/order/address/input")
+	String addressInput(@Validated @ModelAttribute("orderForm") OrderForm form, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("fields", result);
+			session.removeAttribute("orderForm");
+		}
 		return "client/order/address_input";
 	}
 
