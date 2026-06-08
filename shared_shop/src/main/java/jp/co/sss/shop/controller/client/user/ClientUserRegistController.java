1607f8fcfc3d8fd5aef1f473631b59f350ca7ca4
takumika <takumika@C010423114.zipangu.cac.co.jp>
2026-06-08 16:18:13 +0900

    add　アノテーション、フィールドを追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -1,5 +1,12 @@
 package jp.co.sss.shop.controller.client.user;
 
-public class ClientUserRegistController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import jp.co.sss.shop.repository.UserRepository;
+
+@Controller
+public class ClientUserRegistController {
+	@Autowired
+	UserRepository userRepository;
 }
