ee66c1af939632a9caacc38fd1d2a1900bed1894
akariko <akariko@C010423265.zipangu.cac.co.jp>
2026-06-09 10:54:19 +0900

    add カテゴリ検索と並べ替え機能追加

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.entity.Category;
 import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.repository.CategoryRepository;
 import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.service.BeanTools;
 import jp.co.sss.shop.util.Constant;
@@ -30,10 +33,26 @@ public class ClientItemShowController {
 	ItemRepository itemRepository;
 
 	/**
+	 * カテゴリ情報リポジトリ
+	 */
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	/**
 	 * Entity、Form、Bean間のデータコピーサービス
 	 */
 	@Autowired
 	BeanTools beanTools;
+
+	/**
+	 * サイドバー表示用の共通処理
+	 * カテゴリリポジトリの既存メソッドに合わせて修正しました
+	 */
+	@ModelAttribute("categories")
+	public List<Category> getCategories() {
+		// ⭕ 既存の「登録日降順でリスト取得するメソッド」を呼び出す
+		return categoryRepository.findByDeleteFlagOrderByInsertDateDescIdDesc(Constant.NOT_DELETED);
+	}
 
 	/**
 	 * トップ画面 表示処理
@@ -46,16 +65,29 @@ public class ClientItemShowController {
 
 		/*TODO 現在は全件表示を行っている
 		 * これを売れ筋（注文回数が多い順）に改修する*/
-		// 💡ここは「近藤さん」の担当なので、元の状態のまま触らずに残しておきます！
+		// 
 
-		// 注文情報の商品情報を全件表示
-		List<Item> itemList = itemRepository.findAll();
+		// 未削除の「売れ筋順」の商品リストを取得
+		List<Item> itemList = itemRepository.findByDeleteFlagOrderBySalesDesc(Constant.NOT_DELETED);
+		int sortType = 2; //初期値は売れ筋順("2")
+
+		// もし売れ筋順の商品が1件もない場合は、バックアップとして「新着順」で取得
+		if (itemList == null || itemList.isEmpty()) {
+			itemList = itemRepository.findByDeleteFlagOrderByInsertDateDesc(Constant.NOT_DELETED);
+			sortType = 1; // 新着順("1")に切り替え
+		}
+
+		// 3. トップ画面の表示仕様に合わせて「最大10件」にリストを切り出す
+		if (itemList != null && itemList.size() > 10) {
+			itemList = itemList.subList(0, 10);
+		}
 
 		// エンティティ内の検索結果をJavaBeansにコピー
 		List<ItemBean> itemBeanList = beanTools.copyEntityListToItemBeanList(itemList);
 
-		// 商品情報をViewへ渡す
+		// 商品情報と「現在のソート順」Viewへ渡す
 		model.addAttribute("items", itemBeanList);
+		model.addAttribute("sortType", sortType);
 
 		return "index";
 	}
@@ -70,6 +102,11 @@ public class ClientItemShowController {
 	@RequestMapping(path = "/client/item/list/{sortType}", method = { RequestMethod.GET, RequestMethod.POST })
 	public String showItemList(@PathVariable int sortType,
 			@RequestParam(name = "categoryId", required = false) Integer categoryId, Model model) {
+
+		//プルダウンで「指定なし(0)」が選ばれた場合は、カテゴリ指定なし(null)に変換する
+		if (categoryId != null && categoryId == 0) {
+			categoryId = null;
+		}
 
 		List<Item> itemList;
 
