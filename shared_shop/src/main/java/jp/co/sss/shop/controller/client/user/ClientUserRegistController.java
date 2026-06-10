7cbcc100ed703e422a267a959ec20a3a3d9abdf3
takumika <takumika@C010423114.zipangu.cac.co.jp>
2026-06-10 11:47:48 +0900

    add:会員登録機能実装

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.UserForm;
 import jp.co.sss.shop.repository.UserRepository;
 
@@ -45,20 +48,60 @@ public class ClientUserRegistController {
 			model.addAttribute(
 					"org.springframework.validation.BindingResult.userForm",
 					result);
-
 			session.removeAttribute("result");
 		}
 		return "client/user/regist_input";
 	}
 
-	//	@PostMapping(path = "/client/user/regist/check ")
-	//	public String registCheck(HttpSession session, @Valid @ModelAttribute UserForm userForm,
-	//			BindingResult result) {
-	//		if (result.hasErrors()) {
-	//			return "redirect:/client/user/regist_input";
-	//		} else {
-	//			session.setAttribute("userForm", userForm);
-	//			return "client/user/regist_check";
-	//		}
-	//	}
+	@PostMapping(path = "/client/user/regist/check")
+	public String registCheck(HttpSession session, @Valid @ModelAttribute UserForm userForm,
+			BindingResult result) {
+		UserForm lastUserForm = (UserForm) session.getAttribute("userForm");
+		if (lastUserForm == null) {
+			return "redirect:/syserror";
+		}
+		if (userForm.getAuthority() == null) {
+			userForm.setAuthority(lastUserForm.getAuthority());
+		}
+		session.setAttribute("userForm", userForm);
+		session.setAttribute("result", result);
+		if (result.hasErrors()) {
+			return "redirect:/client/user/regist/input";
+		} else {
+			return "redirect:/client/user/regist/check";
+		}
+	}
+
+	@GetMapping(path = "/client/user/regist/check")
+	public String registCheckView(final Model model, final HttpSession session) {
+		UserForm userForm = (UserForm) session.getAttribute("userForm");
+		model.addAttribute("userForm", userForm);
+		return "client/user/regist_check";
+	}
+
+	@PostMapping(path = "/client/user/regist/complete")
+	public String registComplete(final HttpSession session) {
+		UserForm userForm = (UserForm) session.getAttribute("userForm");
+		if (userForm == null) {
+			return "redirect:/syserror";
+		}
+		User user = new User();
+		user.setEmail(userForm.getEmail());
+		user.setPassword(userForm.getPassword());
+		user.setName(userForm.getName());
+		user.setPostalCode(userForm.getPostalCode());
+		user.setAddress(userForm.getAddress());
+		user.setPhoneNumber(userForm.getPhoneNumber());
+		user.setAuthority(userForm.getAuthority());
+		userRepository.save(user);
+		session.removeAttribute("userForm");
+		session.setAttribute("user", user);
+		return "redirect:/client/user/regist/complete";
+	}
+
+	@GetMapping(path = "/client/user/regist/complete")
+	public String registCompleteView() {
+
+		return "client/user/regist_complete";
+	}
 }
