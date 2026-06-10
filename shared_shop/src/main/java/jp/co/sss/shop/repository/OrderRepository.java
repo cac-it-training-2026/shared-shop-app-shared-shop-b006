00de73ba859862217bff9b5d877cd8b7a2ea3bf9
t-hino <t-hino@CD26309L.arksystems.co.jp>
2026-06-10 15:06:48 +0900

    add:一般会員用注文情報検索用リポジトリ

--- shared_shop/src/main/java/jp/co/sss/shop/repository/OrderRepository.java
+++ shared_shop/src/main/java/jp/co/sss/shop/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.sss.shop.entity.Order;
@@ -25,4 +26,12 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 	@Query("SELECT o FROM Order o ORDER BY o.insertDate DESC,o.id DESC")
 	Page<Order> findAllOrderByInsertdateDescIdDesc(Pageable pageable);
 
+	/**
+	 * 【追加】ログイン会員のIDに紐づく注文情報を注文日付降順で検索(一般会員機能で利用)
+	 * * @param userId ログインユーザーのID
+	 * @param pageable ページング情報
+	 * @return 該当会員の注文エンティティのページオブジェクト
+	 */
+	@Query("SELECT o FROM Order o WHERE o.user.id = :userId ORDER BY o.insertDate DESC, o.id DESC")
+	Page<Order> findByUserIdOrderByInsertDateDescIdDesc(@Param("userId") Integer userId, Pageable pageable);
 }
